a6ea8b89135ea06e84578c02650814e0415d1ff1
Ash <krishna.ashwin27@gmail.com>
2017-10-29 17:44:25 -0400

    fixing compilation issues

--- testsortfunc.c
+++ testsortfunc.c
@@ -5,6 +5,6 @@
 int main() {
 	char * filename = "short_movie.csv"; //add filename here
 	FILE* fp = fopen(filename, "r");
-	FILE* fpo = sort_csv(fp, 4);
+	FILE* fpo = sort_csv(fp, filename, 4);
 	return 0;
 }
