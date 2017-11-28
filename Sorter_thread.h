4d2555a6d4729a5f18cc55d81e14bce8bd0797e5
Shaun T. Lim <stl51@strategy.cs.rutgers.edu>
2017-11-27 23:58:31 -0500

    path lock

--- Sorter_thread.h
+++ Sorter_thread.h
@@ -76,13 +76,13 @@ film** mergesort(film** array, int size, int col);//pointer to unsorted array, s
 
 //LOCKSMITH:
 #ifndef insertlock
-pthread_mutex_t insertlock=PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t insertlock=PTHREAD_MUTEX_INITIALIZER;
 #endif
-#ifndef protaglock
-pthread_mutex_t protaglock=PTHREAD_MUTEX_INITIALIZER;
+//#ifndef protaglock
+//pthread_mutex_t protaglock=PTHREAD_MUTEX_INITIALIZER;
 #endif
 #ifndef pathlock
-pthread_mutex_t pathlock=PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t pathlock=PTHREAD_MUTEX_INITIALIZER;
 #endif
 
 
